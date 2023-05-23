2c8b50d268fabdb701008b49b1f71447dfcef560
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-23 08:34:45 -0300

    Update aula2305.py

--- aula2305.py
+++ aula2305.py
@@ -15,7 +15,7 @@ dataframe.style.highlight_max(axis=0)
 st.write(dataframe)
 
 chart_data = pd.DataFrame(
-    np.randon.randon(20, 3),
+    np.random.randon(20, 3),
     columns=['a', 'b', 'c', 'd', 'e'])
    
 st.line_chart(chart_data)
